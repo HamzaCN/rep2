f4f4f6514ad871d22135ceba45153b906de4b50c
HamzaCN <rajpoothamza923@gmail.com>
2020-10-20 23:29:45 +0500

    comiit

---
+++ add.java
@@ -0,0 +1,17 @@
+public class Add{
+
+     public static void main(String []args){
+        int a, b, sum;    //Declaration
+        
+        //Initialization
+        a = 5;
+        b = 10;
+        sum = 0; 
+        cd ..
+        //Expression
+        sum = a + b;
+         
+        //Displaying on Console
+        System.out.println("Sum of a and b is: " + sum);
+     }
+}
